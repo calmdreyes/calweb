387301c32aab6e4d893a27b75e25c7724c3bbc49
calmdreyes <40872037+calmdreyes@users.noreply.github.com>
2018-07-05 14:28:30 -0700

    Create jsscript.js

---
+++ jsscript.js
@@ -0,0 +1,10 @@
+var person = prompt("What's your favorite food?", "Enter anything");
+
+function menu() {
+  var ham = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  }  else {
+    x.className = "topnav";
+  }
+}
